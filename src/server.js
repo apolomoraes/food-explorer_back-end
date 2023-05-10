1c258d8f089a4be79e46351f3c615314e92aed28
Apolo Moraes <vlogsapolo@gmail.com>
2023-05-10 18:54:37 -0300

    serving files

--- src/server.js
+++ src/server.js
@@ -1,14 +1,15 @@
 require("express-async-errors");
 
 const AppError = require("./utils/AppError");
-
 const express = require("express");
-
 const routes = require("./routes/index");
+const uploadConfig = require("./configs/upload");
 
 const app = express();
 app.use(express.json());
 app.use(routes);
+
+app.use("/files", express.static(uploadConfig.UPLOADS_FOLDER))
 
 app.use((error, request, response, next) => {
   if (error instanceof AppError) {
