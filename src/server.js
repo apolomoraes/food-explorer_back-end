9b05297be3b188f36b5db9a55bd49edbcbe2fed1
Apolo Moraes <vlogsapolo@gmail.com>
2023-07-11 16:24:22 -0300

    add .end and pm2

--- src/server.js
+++ src/server.js
@@ -1,4 +1,5 @@
 require("express-async-errors");
+require("dotenv/config");
 
 const AppError = require("./utils/AppError");
 const express = require("express");
@@ -29,7 +30,7 @@ app.use((error, request, response, next) => {
   });
 });
 
-const PORT = 3333;
+const PORT = process.env.SERVER_PORT || 3000;
 app.listen(PORT, () => {
   console.log(`Server is running on Port ${PORT}`)
 }) 
